2c0583dd43910218950f3392eb00fffd0c236359
Binoux <Liebaertb@gmail.com>
2018-02-14 18:56:49 +0100

    Feat(master): Init project
    Init project

---
+++ src/main/java/com/geantcatcountry/geantcat/GeantCatApplication.java
@@ -0,0 +1,12 @@
+package com.geantcatcountry.geantcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeantCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeantCatApplication.class, args);
+	}
+}
